743bd1e7de183df3c54155640eba93a00728d6d1
raylu <90059+raylu@users.noreply.github.com>
2024-11-04 10:28:58 -0800

    ids as JSON

--- sharedassets1.py
+++ sharedassets1.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 
-import csv
+import json
 import pathlib
 
 import bs4
 
-def main():
+def main() -> None:
 	with open('export/sharedassets1.assets.html') as f:
 		soup = bs4.BeautifulSoup(f, 'lxml')
 	table: bs4.Tag = soup.find('body').find('table').find('tbody') # type: ignore
 
+	ids = {}
+	for row in table.find_all('tr'):
+		path_id, class_, name = row.find_all('td')
+		if class_.text != 'MonoBehaviour' or name.text == 'MonoBehaviour':
+			continue
+		ids[int(path_id.text)] = name.text
+
 	data_dir = pathlib.Path('data')
 	data_dir.mkdir(exist_ok=True)
-	with (data_dir / 'ids.csv').open('w') as f:
-		writer = csv.writer(f, lineterminator='\n')
-		for row in table.find_all('tr'):
-			path_id, class_, name = row.find_all('td')
-			if class_.text != 'MonoBehaviour' or name.text == 'MonoBehaviour':
-				continue
-			writer.writerow([path_id.text, name.text])
+	with (data_dir / 'ids.json').open('w') as f:
+		json.dump(ids, f, indent='\t')
 
 if __name__ == '__main__':
 	main()
